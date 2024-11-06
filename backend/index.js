eaf6e186f82779a8c965720c90d9be9092940aed
pthao207203 <149165346+pthao207203@users.noreply.github.com>
2024-11-07 01:33:08 +0700

    update course page admin createCourse

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,10 @@
+const flash = require('express-flash')
 const express = require('express');
 const methodOverride = require('method-override')
+
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser')
+const session = require('express-session')
 
 require('dotenv').config();
 
@@ -19,6 +24,16 @@ app.use(methodOverride('_method'))
 
 app.set('views', './views');
 app.set('view engine', 'pug');
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+app.use(cookieParser('IE104'));
+app.use(session({ cookie: { maxAge: 60000 }}));
+app.use(flash());
 
 // App Locals Variables
 app.locals.prefixAdmin = systemConfig.prefixAdmin;
