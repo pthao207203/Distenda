9c8a6effdddcedea0b956b9a8506c33b0af0528d
tuongvy <22521701@gm.uit.edu.vn>
2024-12-04 23:36:46 +0700

    Update TaskBar

--- frontend/src/layouts/private/TaskBar.jsx
+++ frontend/src/layouts/private/TaskBar.jsx
@@ -3,16 +3,13 @@ import Cookies from "js-cookie";
 
 function TaskBarItem({ text, onClick }) {
     return (
-      <div className="flex gap-3 items-center justify-start px-3 py-5 w-full">
-        <button
+      <button className="flex items-center text-left first-letter:justify-start px-3 py-4 w-full"
           onClick={onClick} // Gọi hàm onClick khi nhấn vào button
-          className="self-stretch my-auto text-left"
           tabIndex="0"
           role="button"
         >
           {text}
-        </button>
-      </div>
+    </button>
 );
 }
 
@@ -35,9 +32,17 @@ function TaskBar() {
   ];
 
   return (
-    <div className="flex flex-col px-5 py-3 justify-center text-xl leading-none text-white bg-gradient-to-b from-[#131313] via-[#1B1B1B] to-[#403F3F] backdrop-blur-[20px]">
+    <div className="flex flex-col px-4 py-3 justify-center text-xl leading-none text-white bg-gradient-to-b from-[#131313]/90 via-[#1B1B1B]/90 to-[#403F3F]/90 backdrop-blur-[20px]">
       {menuItems.map((item, index) => (
-        <TaskBarItem key={index} text={item.text} onClick={item.onClick}  />
+        <div        
+            onMouseEnter={(e) =>
+            (e.currentTarget.style.background = "rgba(0, 0, 0, 0.6)") // Hiệu ứng hover
+            }
+            onMouseLeave={(e) =>
+            (e.currentTarget.style.background = "rgba(0, 0, 0, 0)") // Reset khi rời chuột
+            }>
+        <TaskBarItem key={index} text={item.text} onClick={item.onClick}/>
+        </div>
       ))}
     </div>
   );
