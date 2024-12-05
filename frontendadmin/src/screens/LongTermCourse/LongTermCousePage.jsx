ba5b03a63c01ccf95e9b36026db06495c57db168
Trần Lâm Ngọc Khanh <me123lac@gmail.com>
2024-12-05 19:06:08 +0700

    Add AdminPaymentPage, AdminUserTablePage, AdminUserDetailPage

--- frontendadmin/src/screens/LongTermCourse/LongTermCousePage.jsx
+++ frontendadmin/src/screens/LongTermCourse/LongTermCousePage.jsx
@@ -20,44 +20,43 @@ function CourseForm() {
   ];
 
   return (
-    <main className="flex overflow-hidden flex-col bg-indigo-50 min-h-screen w-full">
-  <NavigationBar />
+    <main className="flex flex-col flex-1 shrink p-16 text-xl font-medium bg-white basis-0 min-w-[240px] max-md:px-5 max-md:max-w-full">
+      <NavigationBar />
+      <section className="flex flex-col px-16 py-8 mt-1.5 bg-white max-md:px-5 max-md:max-w-full w-full">
+        <div className="max-md:max-w-full w-full">
+          <div className="flex gap-8 max-md:flex-col">
+            <div className="flex flex-col w-7/12 max-md:w-full">
+              {formFields.slice(0, 3).map((field, index) => (
+                <FormField key={index} {...field} />
+              ))}
+              <TextArea {...textAreas[0]} />
+            </div>
 
-  <section className="flex flex-col px-16 py-8 mt-1.5 bg-white max-md:px-5 max-md:max-w-full w-full">
-    <div className="max-md:max-w-full w-full">
-      <div className="flex gap-8 max-md:flex-col">
-        <div className="flex flex-col w-7/12 max-md:w-full">
-          {formFields.slice(0, 3).map((field, index) => (
-            <FormField key={index} {...field} />
-          ))}
-          <TextArea {...textAreas[0]} />
+            <div className="flex flex-col w-5/12 max-md:w-full">
+              <button className="flex gap-3 justify-center items-center self-end px-3 py-5 text-3xl text-white rounded-lg bg-slate-500 max-md:mt-5">
+                <img
+                  loading="lazy"
+                  src="https://cdn.builder.io/api/v1/image/assets/ce9d43b270ae41158192dec03af70a1a/9c09cb59c17f1b75dfebbaee00f317fc4e0ec3130264db3528635a1111ca2713?apiKey=7a79403a23cb489f853e4845c47ede19&"
+                  alt=""
+                  className="object-contain shrink-0 self-stretch my-auto w-6 aspect-square"
+                />
+                <span className="gap-2.5 self-stretch my-auto">Tạo khóa học</span>
+              </button>
+              <ImageUpload />
+              <FormField {...formFields[3]} />
+            </div>
+          </div>
         </div>
 
-        <div className="flex flex-col w-5/12 max-md:w-full">
-          <button className="flex gap-3 justify-center items-center self-end px-3 py-5 text-3xl text-white rounded-lg bg-slate-500 max-md:mt-5">
-            <img
-              loading="lazy"
-              src="https://cdn.builder.io/api/v1/image/assets/ce9d43b270ae41158192dec03af70a1a/9c09cb59c17f1b75dfebbaee00f317fc4e0ec3130264db3528635a1111ca2713?apiKey=7a79403a23cb489f853e4845c47ede19&"
-              alt=""
-              className="object-contain shrink-0 self-stretch my-auto w-6 aspect-square"
-            />
-            <span className="gap-2.5 self-stretch my-auto">Tạo khóa học</span>
-          </button>
-          <ImageUpload />
-          <FormField {...formFields[3]} />
+        <div className="flex gap-8 mt-7 max-md:flex-col max-md:max-w-full">
+          <div className="flex flex-col w-7/12 max-md:w-full">
+            <TextArea {...textAreas[1]} />
+          </div>
+          <div className="flex flex-col w-5/12 max-md:w-full">
+            <TextArea {...textAreas[2]} />
+          </div>
         </div>
-      </div>
-    </div>
-
-    <div className="flex gap-8 mt-7 max-md:flex-col max-md:max-w-full">
-      <div className="flex flex-col w-7/12 max-md:w-full">
-        <TextArea {...textAreas[1]} />
-      </div>
-      <div className="flex flex-col w-5/12 max-md:w-full">
-        <TextArea {...textAreas[2]} />
-      </div>
-    </div>
-  </section>
+      </section>
 </main>
 
   );
