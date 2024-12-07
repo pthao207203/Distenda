1bcb48de69241a665ab323bee4ab6a12541a855b
tuongvy <22521701@gm.uit.edu.vn>
2024-12-07 13:53:46 +0700

    Add Forgot Password

--- frontend/src/screens/PublishUser/Login/LoginForm.jsx
+++ frontend/src/screens/PublishUser/Login/LoginForm.jsx
@@ -4,7 +4,7 @@ import LoginButton from './LoginButton';
 
 import { loginController } from '../../../controllers/auth.controller.js';
 
-function LoginForm() {
+function LoginForm({ onForgotPassword }) {
   const [formData, setFormData] = useState({
     UserEmail: '',
     UserPassword: '',
@@ -94,6 +94,11 @@ function LoginForm() {
               onChange={handleChange}
             />
           </div>
+        </div>
+        <div className="flex mt-2 items-center justify-end text-right w-full text-lg max-md:text-[16px]">
+            <button onClick={onForgotPassword} className="flex text-right text-white text-base font-normal hover:font-medium hover:underline self-end my-auto ">
+              Quên mật khẩu
+            </button>
         </div>
         {error && <p className="mt-4 text-red-500">{error}</p>}
         {success && <p className="mt-4 text-[#CFF500]">{success}</p>}
