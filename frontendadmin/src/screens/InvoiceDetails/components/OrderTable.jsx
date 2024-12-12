9c45080a6bb031a37e892862d7ae9d06464ee381
tuongvy <22521701@gm.uit.edu.vn>
2024-12-13 00:44:32 +0700

    Gan API cho payDetail

--- frontendadmin/src/screens/InvoiceDetails/components/OrderTable.jsx
+++ frontendadmin/src/screens/InvoiceDetails/components/OrderTable.jsx
@@ -7,18 +7,18 @@ function OrderTable({ items }) {
       <div className="font-semibold max-md:max-w-full">Chi tiết đơn hàng</div>
       <div className="flex flex-col pb-16 mt-6 w-full font-medium leading-none max-md:max-w-full">
         <div className="flex overflow-hidden flex-wrap w-full rounded-t-3xl bg-[#EBF1F9] min-h-[70px] max-md:max-w-full">
-          <div className="flex flex-1 shrink justify-center items-center text-white bg-[#6C8299] min-w-[240px]">
+          <div className="flex basis-1/3 min-w-0  justify-center items-center text-white bg-[#6C8299]">
             <div className="gap-2.5 self-stretch my-auto">Mã khóa học</div>
           </div>
-          <div className="flex flex-1 shrink justify-center items-center min-w-[240px] text-neutral-900">
+          <div className="flex basis-1/3 min-w-0  justify-center items-center  text-neutral-900">
             <div className="gap-2.5 self-stretch my-auto">Tên khóa</div>
           </div>
-          <div className="flex flex-1 shrink justify-center items-center text-white bg-[#6C8299] min-w-[240px]">
+          <div className="flex basis-1/3 min-w-0 justify-center items-center text-white bg-[#6C8299]">
             <div className="gap-2.5 self-stretch my-auto">Giá</div>
           </div>
         </div>
-        {items.map((item, index) => (
-            <OrderTableRow key={index} item={item} />
+        {items?.map((item, index) => (
+          <OrderTableRow key={index} data={item} />
         ))}
       </div>
     </div>
@@ -26,9 +26,3 @@ function OrderTable({ items }) {
 }
 
 export default OrderTable;
-
-
-
-// {items.map((item, index) => (
-//     <OrderTableRow key={index} item={item} />
-//   ))}
