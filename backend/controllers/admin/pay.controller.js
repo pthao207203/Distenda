84d07c82501f772a66e406a14ec317315639f3e0
Trần Lâm Ngọc Khanh <me123lac@gmail.com>
2025-04-26 23:44:19 +0700

    update momo payment

--- backend/controllers/admin/pay.controller.js
+++ backend/controllers/admin/pay.controller.js
@@ -4,6 +4,75 @@ const Pay = require("../../models/pay.model");
 const Admin = require("../../models/admin.model");
 const Category = require("../../models/category.model");
 
+const axios = require('axios');
+const crypto = require('crypto');
+const momoConfig = require("../../config/momo.config");
+
+module.exports.payMomo = async (req, res) => {
+  if (!req.cookies.user_token) {
+    return res.json({
+      code: 401,
+      message: "Bạn chưa đăng nhập!"
+    });
+  }
+
+  const course = await Course.findOne({
+    CourseSlug: req.params.CourseSlug
+  });
+  if (!course) {
+    return res.json({
+      code: 404,
+      message: "Không tìm thấy khóa học!"
+    });
+  }
+
+  const amount = course.CoursePrice * (100 - course.CourseDiscount) / 100;
+  const orderId = momoConfig.partnerCode + new Date().getTime();
+  const requestId = orderId;
+
+  const rawSignature =
+  `accessKey=${momoConfig.accessKey}&amount=${amount}&extraData=${momoConfig.extraData}&ipnUrl=${momoConfig.ipnUrl}&orderId=${orderId}&orderInfo=Thanh toán khoá học ${course.CourseName}&partnerCode=${momoConfig.partnerCode}&redirectUrl=${momoConfig.redirectUrl}&requestId=${requestId}&requestType=${momoConfig.requestType}`;
+
+  const signature = crypto.createHmac('sha256', momoConfig.secretKey).update(rawSignature).digest('hex');
+  const dynamicRedirectUrl = `http://localhost:3000/courses/${course.CourseSlug}?success=1`;
+
+  const requestBody = {
+    partnerCode: momoConfig.partnerCode,
+    partnerName: 'Distenda',
+    storeId: 'DistendaStore',
+    requestId: requestId,
+    amount: `${amount}`,
+    orderId: orderId,
+    orderInfo: `Thanh toán khoá học ${course.CourseName}`,
+    redirectUrl: momoConfig.redirectUrl,
+    ipnUrl: momoConfig.ipnUrl,
+    lang: momoConfig.lang,
+    requestType: momoConfig.requestType,
+    autoCapture: momoConfig.autoCapture,
+    extraData: momoConfig.extraData,
+    signature: signature,
+  };
+
+  try {
+    const response = await axios.post('https://test-payment.momo.vn/v2/gateway/api/create', requestBody, {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    return res.json({
+      code: 200,
+      payUrl: response.data.payUrl
+    });
+  } catch (err) {
+    return res.json({
+      code: 500,
+      message: "Lỗi khi kết nối MoMo",
+      error: err.message
+    });
+  }
+};
+
+
 // // [GET] /admin/pay/
 module.exports.pay = async (req, res) => {
   const pays = await Pay.find().lean()
